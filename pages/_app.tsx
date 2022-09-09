6609f822e372b3da711e539927edfffc6de98a5b
Aryan <aryan.pathania2003@gmail.com>
2022-09-09 15:18:53 +0530

    Created Homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import { ThemeProvider } from 'next-themes';
 
-export default MyApp;
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
