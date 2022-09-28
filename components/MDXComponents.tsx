cf71ddbd0a09eaafaa766312216c6aff52123066
Aryan <aryan.pathania2003@gmail.com>
2022-09-29 02:36:39 +0530

    ✨ Added MDX Components and passed them to MDXProvider

---
+++ components/MDXComponents.tsx
@@ -0,0 +1,6 @@
+/**
+ * This file stores all the components that are to be used in the MDX files.
+ * This components are passed to MDXProvider in `_pages/_app.tsx`
+ */
+const MDXComponents = {};
+export default MDXComponents;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-
+import { MDXProvider } from '@mdx-js/react';
 import { ThemeProvider } from 'next-themes';
+import MDXComponents from '../components/MDXComponents';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <MDXProvider components={MDXComponents}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </MDXProvider>
   );
 }
