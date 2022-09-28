4f845b3c644f03ab456d9f8044bbb9019bd0579d
Aryan <aryan.pathania2003@gmail.com>
2022-09-28 06:19:51 +0530

    🔧 Added `@next/mdx`

--- next.config.js
+++ next.config.js
@@ -1,8 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const mdx = require('@next/mdx');
+
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    providerImportSource: '@mdx-js/react',
+  },
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { images: { allowFutureImage: true } },
 };
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  ...withMDX({
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  }),
+};
