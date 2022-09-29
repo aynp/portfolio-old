ad80392ecfee67729a6e90d72efae22721c7403f
Aryan <aryan.pathania2003@gmail.com>
2022-09-29 07:41:39 +0530

    🔧 Added `lang` attribute

--- next.config.js
+++ next.config.js
@@ -15,6 +15,10 @@ const withMDX = mdx({
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
   experimental: { images: { allowFutureImage: true } },
 };
 
