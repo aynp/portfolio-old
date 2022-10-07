63bf28dfeb3345ad1bccf16cc5851d4566459154
Aryan <aryan.pathania2003@gmail.com>
2022-10-07 07:52:11 +0530

    ✨ Added helper function to get posts as props

--- lib/notion.ts
+++ lib/notion.ts
@@ -32,3 +32,37 @@ export const getBlocks = async (blockId: string) => {
   }
   return blocks;
 };
+
+export async function getPostProps(id: string) {
+  const page = await getPage(id);
+  const blocks = await getBlocks(id);
+
+  // Retrieve block children for nested blocks (one level deep), for example toggle blocks
+  // https://developers.notion.com/docs/working-with-page-content#reading-nested-blocks
+  const childBlocks = await Promise.all(
+    blocks
+      .filter((block) => block.has_children)
+      .map(async (block) => {
+        return {
+          id: block.id,
+          children: await getBlocks(block.id),
+        };
+      })
+  );
+  const blocksWithChildren = blocks.map((block) => {
+    // Add child blocks if the block should contain children but none exists
+    if (block.has_children && !block[block.type].children) {
+      block[block.type]['children'] = childBlocks.find(
+        (x) => x.id === block.id
+      )?.children;
+    }
+    return block;
+  });
+
+  return {
+    props: {
+      page,
+      blocks: blocksWithChildren,
+    },
+  };
+}
