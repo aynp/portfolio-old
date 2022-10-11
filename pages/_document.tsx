5b0acfb5fd804509a29ee871d2e17227a44c2740
Aryan <aryan.pathania2003@gmail.com>
2022-10-11 15:18:40 +0530

    ⚡ Preload fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/ibm-plex-sans-var.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body className="bg-white dark:bg-black text-white dark:text-black">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
