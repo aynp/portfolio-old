bcc0573dcd720710e283cb502ea705ce0a8b3d07
Aryan <aryan.pathania2003@gmail.com>
2022-10-29 14:50:46 +0530

    ✨ Add root `layout`

---
+++ app/layout.tsx
@@ -0,0 +1,180 @@
+'use client';
+
+import '/styles/globals.css';
+import Head from 'next/head';
+import { usePathname } from 'next/navigation';
+import { useState, useEffect } from 'react';
+import { useTheme } from 'next-themes';
+import Link from 'next/link';
+import NextLink from 'next/link';
+
+import cn from 'classnames';
+
+import MobileMenu from './MobileMenu';
+
+function NavItem({ href, text }: any) {
+  const currPath = usePathname();
+  const isActive = currPath === href;
+
+  return (
+    <NextLink
+      href={href}
+      className={cn(
+        isActive
+          ? 'font-semibold text-gray-800 dark:text-gray-200'
+          : 'font-normal text-gray-600 dark:text-gray-400',
+        'hidden md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all'
+      )}>
+      <span className={cn(isActive ? 'active-menu-item' : '', 'capsize')}>
+        {text}
+      </span>
+    </NextLink>
+  );
+}
+
+const ExternalLink = ({ href, children }: any) => (
+  <a
+    className="text-gray-500 hover:text-gray-600 transition"
+    target="_blank"
+    rel="noopener noreferrer"
+    href={href}>
+    {children}
+  </a>
+);
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
+
+  // After mounting, we have access to the theme
+  useEffect(() => setMounted(true), []);
+
+  const meta = {
+    title: 'Aryan Pathania - Portfolio',
+    description: `Computer Science and Engineering Student`,
+    image: '/social-banner.png',
+    type: 'website',
+  };
+
+  return (
+    <div className="bg-gray-50 dark:bg-gray-900">
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        {/* <meta property="og:url" content={`https://aryanpathania.vercel.app${router.asPath}`} /> */}
+        {/* <link rel="canonical" href={`https://aryanpathania.vercel.app${router.asPath}`} /> */}
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Aryan Pathania" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@aryanpathania03" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+      </Head>
+      <div className="flex flex-col justify-center px-8">
+        <nav className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
+          <a href="#skip" className="skip-nav">
+            Skip to content
+          </a>
+          <div className="ml-[-0.60rem]">
+            <MobileMenu />
+            <NavItem href="/" text="Home" />
+            <NavItem href="/timeline" text="Timeline" />
+            <NavItem href="/blog" text="Blog" />
+            <NavItem href="/projects" text="Projects" />
+            <NavItem href="/snippets" text="Snippets" />
+            <NavItem href="/about" text="About" />
+          </div>
+          <button
+            aria-label="Toggle Dark Mode"
+            type="button"
+            className="w-9 h-9 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all"
+            onClick={() =>
+              setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+            }>
+            {mounted && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                className="w-5 h-5 text-gray-800 dark:text-gray-200">
+                {resolvedTheme === 'dark' ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                  />
+                )}
+              </svg>
+            )}
+          </button>
+        </nav>
+      </div>
+      <main
+        id="skip"
+        className="flex flex-col justify-center px-8 bg-gray-50 dark:bg-gray-900">
+        {children}
+      </main>
+      <footer className="flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8">
+        <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
+        <div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
+          <div className="flex flex-col space-y-4">
+            <Link
+              href="/"
+              className="text-gray-500 hover:text-gray-600 transition">
+              Home
+            </Link>
+            <Link
+              href="/blog"
+              className="text-gray-500 hover:text-gray-600 transition">
+              Blog
+            </Link>
+            <Link
+              href="/about"
+              className="text-gray-500 hover:text-gray-600 transition">
+              About
+            </Link>
+          </div>
+          <div className="flex flex-col space-y-4">
+            <ExternalLink href="https://twitter.com/aryanpathania03">
+              Twitter
+            </ExternalLink>
+            <ExternalLink href="https://github.com/aynp">GitHub</ExternalLink>
+            <ExternalLink href="https://linkedin.com/in/aryanpathania03/">
+              LinkedIn
+            </ExternalLink>
+          </div>
+          <div className="flex flex-col space-y-4">
+            <Link
+              href="/projects"
+              className="text-gray-500 hover:text-gray-600 transition">
+              Projects
+            </Link>
+            <Link
+              href="/snippets"
+              className="text-gray-500 hover:text-gray-600 transition">
+              Snippets
+            </Link>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
